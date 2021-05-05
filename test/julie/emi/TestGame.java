88f3f3cc56b32c731332163ae2c2f1e1932404ef
JulieM-dev <Julie.neko.egirl@gmail.com>
2021-05-05 23:17:16 +0200

    ajouts

---
+++ test/julie/emi/TestGame.java
@@ -0,0 +1,16 @@
+package julie.emi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGame
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
